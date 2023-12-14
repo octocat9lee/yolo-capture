f033c64b1f41bec06e9ba1e3c55baef9ea00150a
octocat9lee <octocat9lee@gmail.com>
2023-12-14 10:22:52 +0800

    format the code using clang-format style

--- src/spd_log.h
+++ src/spd_log.h
@@ -31,11 +31,13 @@ private:
     void SetLevel(const std::string &level);
 };
 
-#define SPD_TRACE(...) SpdLog::Instance().Logger().get()->trace(__VA_ARGS__)
-#define SPD_DEBUG(...) SpdLog::Instance().Logger().get()->debug(__VA_ARGS__)
-#define SPD_INFO(...) SpdLog::Instance().Logger().get()->info(__VA_ARGS__)
-#define SPD_WARN(...) SpdLog::Instance().Logger().get()->warn(__VA_ARGS__)
-#define SPD_ERROR(...) SpdLog::Instance().Logger().get()->error(__VA_ARGS__)
-#define SPD_CRITICAL(...) SpdLog::Instance().Logger().get()->critical(__VA_ARGS__)
+// clang-format off
+#define SPD_TRACE(...)     SpdLog::Instance().Logger().get()->trace(__VA_ARGS__)
+#define SPD_DEBUG(...)     SpdLog::Instance().Logger().get()->debug(__VA_ARGS__)
+#define SPD_INFO(...)      SpdLog::Instance().Logger().get()->info(__VA_ARGS__)
+#define SPD_WARN(...)      SpdLog::Instance().Logger().get()->warn(__VA_ARGS__)
+#define SPD_ERROR(...)     SpdLog::Instance().Logger().get()->error(__VA_ARGS__)
+#define SPD_CRITICAL(...)  SpdLog::Instance().Logger().get()->critical(__VA_ARGS__)
+// clang-format on
 
 #endif // SPD_LOG_H
